771c205ab4ddb893f5518e0811e56e4a411a6dc6
Jubayer husain <jubayerhusai360@gmail.com>
2024-12-06 20:42:30 +0600

    environment variable done

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -3,12 +3,18 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvhUNgpG0c_NjvGyGt7tcdj2hiGNjrTgw",
-  authDomain: "equipify-603ef.firebaseapp.com",
-  projectId: "equipify-603ef",
-  storageBucket: "equipify-603ef.firebasestorage.app",
-  messagingSenderId: "952794093188",
-  appId: "1:952794093188:web:075c3ee8e8c2dc1101d4d1"
+  // apiKey: "AIzaSyDvhUNgpG0c_NjvGyGt7tcdj2hiGNjrTgw",
+  // authDomain: "equipify-603ef.firebaseapp.com",
+  // projectId: "equipify-603ef",
+  // storageBucket: "equipify-603ef.firebasestorage.app",
+  // messagingSenderId: "952794093188",
+  // appId: "1:952794093188:web:075c3ee8e8c2dc1101d4d1"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
